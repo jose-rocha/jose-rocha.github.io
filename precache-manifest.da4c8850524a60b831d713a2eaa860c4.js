23f84f9561fafc05b2890b5ef2d67d084f019eec
Anonymous <joserocha@pop-os.localdomain>
2020-06-09 21:33:20 -0300

    subindo site de abrponto

---
+++ precache-manifest.da4c8850524a60b831d713a2eaa860c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e25bf0719a85f6c977cb6b65121a04c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "deb40b52cf65944e5cf5",
+    "url": "/static/css/main.4414ec38.chunk.css"
+  },
+  {
+    "revision": "9cf6c8e7b18d98412b49",
+    "url": "/static/js/2.81ce8afb.chunk.js"
+  },
+  {
+    "revision": "3bf522dbcf2126e372d60f66eecc8197",
+    "url": "/static/js/2.81ce8afb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deb40b52cf65944e5cf5",
+    "url": "/static/js/main.6b827aea.chunk.js"
+  },
+  {
+    "revision": "18c3e0e655bb937b7b8b",
+    "url": "/static/js/runtime-main.7201c2f2.js"
+  }
+]);
